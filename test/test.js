43ba398e08f65cc9973d3a06bdc980ad5d1e64b9
Mikola Lysenko <mikolalysenko@gmail.com>
2013-04-10 10:36:54 -0500

    added more test cases, debugged some issues with vertices fusing improperly

--- test/test.js
+++ test/test.js
@@ -1,11 +1,19 @@
 var cubeMesh = require("../cube.js")
+  , euler = require("euler-characteristic")
 
 require("tap").test("cube", function(t) {
   
-  var cube = cubeMesh(1, 1.0)
+  var empty_cube = cubeMesh(0)
+  t.equals(empty_cube.positions.length, 0)
+  t.equals(empty_cube.cells.length, 0)
   
-  t.equals(cube.positions.length, 8)
-  t.equals(cube.cells.length, 12)
+  for(var i=1; i<10; ++i) {
+    var cube = cubeMesh(i, 1.0)
+    
+    t.equals(cube.positions.length, 8 + 12 * (i-1) + 6*(i-1)*(i-1))
+    t.equals(cube.cells.length, 2 * 6 * i * i)
+    t.equals(euler(cube.cells), 2)
+  }
   
   t.end()
 })
